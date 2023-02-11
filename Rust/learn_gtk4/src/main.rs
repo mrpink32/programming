32a8e623e492c76c339e65131ec156ccfbf948f2
mikkel <mikkelpaulick@hotmail.com>
2023-02-11 15:23:29 +0100

    list view stuff

--- Rust/learn_gtk4/src/main.rs
+++ Rust/learn_gtk4/src/main.rs
@@ -1,8 +1,19 @@
 mod custom_widgets;
 
-use custom_widgets::CustomButton;
+use custom_widgets::{CustomButton, IntegerObject, Window};
+use gtk4::CustomFilter;
+use gtk4::CustomSorter;
+use gtk4::FilterListModel;
+use gtk4::ListItem;
+use gtk4::ListView;
+use gtk4::ScrolledWindow;
+use gtk4::SelectionModel;
+use gtk4::SignalListItemFactory;
+use gtk4::SingleSelection;
+use gtk4::SortListModel;
+use gtk4::StringList;
 use gtk4::{
-    gio::{Settings, SettingsBindFlags},
+    gio::{ListStore, Settings, SettingsBindFlags},
     glib,
     glib::{
         clone, closure_local, signal::Inhibit, BindingFlags, GString, MainContext, Object,
@@ -10,7 +21,7 @@ use gtk4::{
     },
     prelude::*,
     subclass::prelude::*,
-    Application, ApplicationWindow, Button, Label, Orientation, Switch,
+    Application, ApplicationWindow, Button, Label, Orientation, Switch, Widget,
 };
 use std::{cell::Cell, rc::Rc, thread, time::Duration};
 
@@ -39,6 +50,95 @@ fn build_ui(application: &Application) {
     let settings: Settings = Settings::with_path(APP_ID, "/org/gtk_rs/HelloWorld/");
     // // Get the last switch state from the settings
     // let is_switch_enabled: bool = settings.boolean("is-switch-enabled");
+
+    // Create a `Vec<IntegerObject>` with numbers from 0 to 100_000
+    let vector: Vec<IntegerObject> = (0..=1_000_000)
+        .into_iter()
+        .map(IntegerObject::new)
+        .collect();
+
+    // Create a new model
+    let model: ListStore = ListStore::new(IntegerObject::static_type());
+
+    // Add the vector to the model
+    model.extend_from_slice(&vector);
+
+    // Create a new model
+    // let model: StringList = (0..=1_000_000)
+    //     .into_iter()
+    //     .map(|number: i32| number.to_string())
+    //     .collect();
+
+    let factory: SignalListItemFactory = SignalListItemFactory::new();
+    factory.connect_setup(move |_, list_item: &ListItem| {
+        let label: Label = Label::new(None);
+        let list_item = list_item
+            .downcast_ref::<ListItem>()
+            .expect("Needs to be ListItem");
+        list_item.set_child(Some(&label));
+        // Bind `list_item->item->number` to `label->label`
+        list_item
+            .property_expression("item")
+            .chain_property::<IntegerObject>("number")
+            // .chain_property::<IntegerObject>("string")
+            .bind(&label, "label", Widget::NONE);
+    });
+
+    let filter: CustomFilter = CustomFilter::new(move |obj| {
+        // Get `IntegerObject` from `glib::Object`
+        let integer_object: &IntegerObject = obj
+            .downcast_ref::<IntegerObject>()
+            .expect("The object needs to be of type `IntegerObject`.");
+
+        // Get property "number" from `IntegerObject`
+        let number: i32 = integer_object.property::<i32>("number");
+
+        // Only allow even numbers
+        number % 2 == 0
+    });
+    let filter_model: FilterListModel = FilterListModel::new(Some(&model), Some(&filter));
+
+    let sorter: CustomSorter = CustomSorter::new(move |obj1, obj2| {
+        // Get `IntegerObject` from `glib::Object`
+        let integer_object_1 = obj1
+            .downcast_ref::<IntegerObject>()
+            .expect("The object needs to be of type `IntegerObject`.");
+        let integer_object_2 = obj2
+            .downcast_ref::<IntegerObject>()
+            .expect("The object needs to be of type `IntegerObject`.");
+
+        // Get property "number" from `IntegerObject`
+        let number_1 = integer_object_1.property::<i32>("number");
+        let number_2 = integer_object_2.property::<i32>("number");
+
+        // Reverse sorting order -> large numbers come first
+        number_2.cmp(&number_1).into()
+    });
+    let sort_model: SortListModel = SortListModel::new(Some(&filter_model), Some(&sorter));
+
+    // factory.connect_bind(move |_, list_item: &ListItem| {
+    //     // Get `IntegerObject` from `ListItem`
+    //     let integer_object = list_item
+    //         .downcast_ref::<ListItem>()
+    //         .expect("Needs to be ListItem")
+    //         .item()
+    //         .and_downcast::<IntegerObject>()
+    //         .expect("The item has to be an `IntegerObject`.");
+
+    //     // Get `i32` from `IntegerObject`
+    //     let number: i32 = integer_object.property::<i32>("number");
+
+    //     // Get `Label` from `ListItem`
+    //     let label: Label = list_item
+    //         .downcast_ref::<ListItem>()
+    //         .expect("Needs to be ListItem")
+    //         .child()
+    //         .and_downcast::<Label>()
+    //         .expect("The child has to be a `Label`.");
+
+    //     // Set "label" to "number"
+    //     label.set_label(&number.to_string());
+    // });
 
     // Create a custom button
     let custom_button: CustomButton = CustomButton::new();
@@ -203,6 +303,12 @@ fn build_ui(application: &Application) {
             label.set_label(format!("Counter: {}", &number.get()).as_str());
     }));
 
+    let list_box: gtk4::ListBox = gtk4::ListBox::new();
+    for number in 0..=100_000 {
+        let number_label: Label = Label::new(Some(&number.to_string()));
+        list_box.append(&number_label);
+    }
+
     // Add buttons to `gtk_box`
     let gtk_box: gtk4::Box = gtk4::Box::builder()
         .orientation(Orientation::Vertical)
@@ -217,11 +323,45 @@ fn build_ui(application: &Application) {
     gtk_box.append(&button_decrease);
     gtk_box.append(&label);
 
+    // // Create custom window
+    // let window = Window::new(application);
+    // window.set_title(Some("My GTK App"));
+    // // Add button
+    // window.set_child(Some(&gtk_box));
+
+    let selection_model: SingleSelection = SingleSelection::new(Some(&sort_model));
+    let list_view: ListView = ListView::new(Some(&selection_model), Some(&factory));
+
+    list_view.connect_activate(move |list_view: &ListView, position: u32| {
+        // Get `IntegerObject` from model
+        let model = list_view.model().expect("The model has to exist.");
+        let integer_object = model
+            .item(position)
+            .and_downcast::<IntegerObject>()
+            .expect("The item has to be an `IntegerObject`.");
+
+        // Increase "number" of `IntegerObject`
+        integer_object.increase_number();
+
+        // Notify that the filter and sorter have been changed
+        filter.changed(gtk4::FilterChange::Different);
+        sorter.changed(gtk4::SorterChange::Different);
+    });
+
+    // Create a window
+    let scrolled_window: ScrolledWindow = ScrolledWindow::builder()
+        .hscrollbar_policy(gtk4::PolicyType::Never)
+        .min_content_width(360)
+        .child(&list_view)
+        .build();
+
     // Create a window
     let window: ApplicationWindow = ApplicationWindow::builder()
         .application(application)
         .title("My GTK App")
-        .child(&gtk_box)
+        .default_width(600)
+        .default_height(300)
+        .child(&scrolled_window)
         .build();
 
     // Present window
