55b0753de232ba8f1d9e371fb9f79b82cc2f4b9d
mikkel <mikkelpaulick@hotmail.com>
2023-02-20 08:47:58 +0100

    basic mandelbrot plotter

--- Rust/mandelbrot/src/main.rs
+++ Rust/mandelbrot/src/main.rs
@@ -1,6 +1,6 @@
+use image::{codecs::png::PngEncoder, ColorType, ImageEncoder};
 use num::Complex;
-use std::env;
-use std::str::FromStr;
+use std::{env, fs::File, str::FromStr};
 
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     let mut z: Complex<f64> = Complex { re: 0.0, im: 0.0 };
@@ -13,7 +13,7 @@ fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     None
 }
 
-fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<T, T> {
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     match s.find(separator) {
         None => None,
         Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
@@ -23,21 +23,113 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<T, T> {
     }
 }
 
-fn main() {
-    println!("Hello, world!");
-    let mut numbers: Vec<f64> = Vec::new();
-    for arg in env::args().skip(1) {
-        numbers.push(arg.parse::<f64>().expect("Failed conversion!"));
+fn parse_complex(string: &str) -> Option<Complex<f64>> {
+    match parse_pair(string, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None,
     }
+}
 
-    if numbers.len() == 0 {
-        eprintln!("No arguments given...");
+#[test]
+fn test_parse_complex() {
+    assert_eq!(
+        parse_complex("1.25,-0.0625"),
+        Some(Complex {
+            re: 1.25,
+            im: -0.0625,
+        })
+    );
+    assert_eq!(parse_complex(",-0.0625"), None);
+}
+
+fn pixel_to_point(
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) -> Complex<f64> {
+    let (width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
+
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
+    }
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(
+        pixel_to_point(
+            (100, 200),
+            (25, 175),
+            Complex { re: -1.0, im: 1.0 },
+            Complex { re: 1.0, im: -1.0 },
+        ),
+        Complex {
+            re: -0.5,
+            im: -0.75,
+        }
+    );
+}
+
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert_eq!(pixels.len(), bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point: Complex<f64> =
+                pixel_to_point(bounds, (column, row), upper_left, lower_right);
+
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            };
+        }
+    }
+}
+
+fn write_image(
+    filename: &str,
+    pixels: &[u8],
+    bounds: (usize, usize),
+) -> Result<(), std::io::Error> {
+    let output: File = File::create(filename)?;
+
+    let encoder: PngEncoder<File> = PngEncoder::new(output);
+    encoder.write_image(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::L8).expect("TODO: panic message");
+
+    Ok(())
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect::<Vec<String>>();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!(
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        );
         std::process::exit(1);
     }
 
-    let mut d: f64 = numbers[0];
-    for m in &mut numbers[1..] {
-        d = d + *m;
-    }
-    println!("{}", d);
+    let bounds = parse_pair(&args[2], 'x').expect("Error parsing image dimensions!");
+    let upper_left: Complex<f64> =
+        parse_complex(&args[3]).expect("Error parsing upper left corner point!");
+    let lower_right: Complex<f64> =
+        parse_complex(&args[4]).expect("Error parsing lower right corner point!");
+
+    let mut pixels: Vec<u8> = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds).expect("Error writing PNG file!");
 }
