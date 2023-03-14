6b91e220260359063ccb0c7b9f2ca2aec175003d
mikkel <mikkelpaulick@hotmail.com>
2023-03-15 00:54:39 +0100

    dunno

--- Rust/taylorpolynomier/src/main.rs
+++ Rust/taylorpolynomier/src/main.rs
@@ -20,8 +20,8 @@ fn taylor_sqrt(x: f64) -> f64 {
                        // sqrt(4) = 2.0
     let a1: f64 = 0.5 * ((x - 1_f64).powi(1) / 1.factorial()); // 1 / (2 * sqrt(1)) = 1/2 = 0.5
                                                                // let a1: f64 = 0.25 * ((x - 4_f64).powi(1) / 1.factorial()); // 1 / (2 * sqrt(4)) = 1/4 = 0.25
-    let a2: f64 = -0.25 * ((x - 1_f64).powi(2) / 2.factorial()); // -1 / (4 * sqrt(1)^3) = -1/4 = -0.25
-                                                                 // let a2: f64 = -0.03125 * ((x - 4_f64).powi(2) / 2.factorial()); // -1 / (4 * sqrt(4)^3) = -0.03125
+    let a2: f64 = (-0.25) * ((x - 1_f64).powi(2) / 2.factorial()); // -1 / (4 * sqrt(1)^3) = -1/4 = -0.25
+                                                                   // let a2: f64 = -0.03125 * ((x - 4_f64).powi(2) / 2.factorial()); // -1 / (4 * sqrt(4)^3) = -0.03125
     let a3: f64 = 0.375 * ((x - 1_f64).powi(3) / 3.factorial()); // 3 / (8 * sqrt(1)^5) = 3/8 = 0.375
                                                                  // let a3: f64 =  // 3 / (8 * sqrt(4)^5) = 0.375
 
@@ -32,19 +32,19 @@ fn taylor_sqrt(x: f64) -> f64 {
 fn taylor_cos(x: f64, n: Option<i32>) -> f64 {
     let a0: f64 = 1.0; // cos(0) = 1
     let a1: f64 = 0.0 * (x.powi(1) / 1.factorial()); // -sin(0) = 0
-    let a2: f64 = -1.0 * (x.powi(2) / 2.factorial()); // -0.5 * x.powf(2_f64); // -cos(0) = -1
+    let a2: f64 = (-1.0) * (x.powi(2) / 2.factorial()); // -0.5 * x.powf(2_f64); // -cos(0) = -1
     let a3: f64 = 0.0 * (x.powi(3) / 3.factorial()); // sin(0) = 0
     let a4: f64 = 1.0 * (x.powi(4) / 4.factorial()); // cos(0) = 1
     let a5: f64 = 0.0 * (x.powi(5) / 5.factorial()); // -sin(0) = 0
-    let a6: f64 = -1.0 * (x.powi(6) / 6.factorial()); // -cos(0) = -1
+    let a6: f64 = (-1.0) * (x.powi(6) / 6.factorial()); // -cos(0) = -1
     let a7: f64 = 0.0 * (x.powi(7) / 7.factorial()); // sin(0) = 0
     let a8: f64 = 1.0 * (x.powi(8) / 8.factorial()); // cos(0) = 1
     let a9: f64 = 0.0 * (x.powi(9) / 9.factorial()); // -sin(0) = 0
-    let a10: f64 = -1.0 * (x.powi(10) / 10.factorial()); // -cos(0) = -1
+    let a10: f64 = (-1.0) * (x.powi(10) / 10.factorial()); // -cos(0) = -1
     let a11: f64 = 0.0 * (x.powi(11) / 11.factorial()); // sin(0) = 0
     let a12: f64 = 1.0 * (x.powi(12) / 12.factorial()); // cos(0) = 1
     let a13: f64 = 0.0 * (x.powi(13) / 13.factorial()); // -sin(0) = 0
-    let a14: f64 = -1.0 * (x.powi(14) / 14.factorial()); // -cos(0) = -1
+    let a14: f64 = (-1.0) * (x.powi(14) / 14.factorial()); // -cos(0) = -1
     let a15: f64 = 0.0 * (x.powi(15) / 15.factorial()); // sin(0) = 0
     let result: f64 =
         a0 + a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 + a10 + a11 + a12 + a13 + a14 + a15;
